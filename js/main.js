f5def1329d0dd59b746f9bfb49d8faa8a7813d02
NeiTDutra <nei.thomass@gmail.com>
2021-09-01 14:16:42 -0300

    implementa efeito fade da seção sobre mim

--- js/main.js
+++ js/main.js
@@ -19,3 +19,19 @@ $('#recipeCarousel').carousel({
           next.children(':first-child').clone().appendTo($(this));
         }
   });
+
+$(document).scroll(function(){
+
+  if( $('#sobre-min').scrollTop() > $('#sobre-min').height()/2 ) {
+    
+    $('#title-sobre-mim').addClass('opacity-zero-to-one');
+    $('#p1-sobre-mim #p3-sobre-mim').addClass('l-t-r');
+    $('#p2-sobre-mim #p4-sobre-mim').addClass('r-t-l');
+  }
+  else {
+    
+    $('#title-sobre-mim').removeClass('opacity-zero-to-one');
+    $('#p1-sobre-mim #p3-sobre-mim').removeClass('l-t-r');
+    $('#p2-sobre-mim #p4-sobre-mim').removeClass('r-t-l');
+  }
+});
